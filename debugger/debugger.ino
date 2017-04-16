073b8650b677f3b4b096e03e677b7a8937a5e16e
Diwakar Ganesan <diwgan32@DN0a22b31c.SUNet>
2017-04-15 19:55:25 -0700

    Wrote more debugger code

--- debugger/debugger.ino
+++ debugger/debugger.ino
@@ -14,16 +14,46 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     unsigned char info = Serial.read();
+
+    // Read Program Counter From Digital Bus
     if(info == 48){
-      
+      Serial.print("Printing program counter: ");
       digitalWrite(CLOCK_PIN, HIGH);
       digitalWrite(CLOCK_PIN, LOW);
       unsigned char pc = 0;
       for(int i = 7; i>=0; i--){
-         pc |= (digitalRead(dataPins[i]) << i)
+         pc |= (digitalRead(dataPins[i]) << i);
       }
       Serial.println(pc);
     }
+    if(info == 49){
+      Serial.print("Printing Registers...");
+      for(int i = 7; i>=0; i--){
+          pinMode(dataPins[i], OUTPUT);
+      }
+
+      for (int k = 0; k < 4; k++) {  // Do this for 4 registers.
+        unsigned char instr = 50 + k;
+        for(int i = 7; i>=0; i--){
+           digitalWrite(dataPins[i], instr & (1 << i));
+        }
+
+        for(int i = 7; i>=0; i--){
+           pinMode(dataPins[i], OUTPUT);
+        }
+        unsigned char reg = 0;
+        for(int i = 7; i>=0; i--){
+          reg |= (digitalRead(dataPins[i]) << i);
+        }
+        Serial.print("r");
+        Serial.print(k);
+        Serial.print(": ");
+        Serial.println(reg);
+      }
+      
+    }
+    
+    
     
   }
 
