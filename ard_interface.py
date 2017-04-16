7f3e564ab7f721d88b3fc316f9ab402f0bc86e40
aAnthony <degleris1@gmail.com>
2017-04-15 17:46:29 -0700

    created simple test program

--- ard_interface.py
+++ ard_interface.py
@@ -9,48 +9,59 @@ ARD_NAME = '/dev/cu.wchusbserial1420'
 BITRATE = 9600
 
 ard = ser.Serial(ARD_NAME, BITRATE, timeout=None)
-cmd = sys.argv[1]
-time.sleep(3);
+time.sleep(3)
 
-def read(num_bytes):
+def convert(num):
+    if (num.startswith('0x')):
+        return int(num, 16)
+    else
+        return int(num)
+
+def read(num_bytes, address):
     
-    ard.write('\x01')  # notfiy reading
-    ard.write('\x00')  # notfiy reading
+    ard.write(chr(1))  # notfiy reading
+    ard.write(chr(address))  # notfiy address
     ard.write(chr(num_bytes))  # specify size
 
-    time.sleep(.01);
+    time.sleep(.01)
 
     for i in range(0, num_bytes):
         print(ard.read())
         time.sleep(0.002)
-        
-    #print(ard.read(num_bytes))
 
-
-def write():
-    file_name = sys.argv[2]  # get file
-
+def write(file_name, address):
     f = open(file_name, 'rb')
     data = f.read()
 
-    ard.write('\x00')  # notify writing
-    ard.write('\x00')
+    ard.write(chr(0))  # notify writing
+    ard.write(chr(address))  # specify address
     ard.write(chr(len(data)))  # specify size
+    
     for c in data:
         ard.write(c)
-
 
 def clear():
     ard.write(10)
 
 
-if (cmd == '-r'):
-    n = int(sys.argv[2])
-    read(n)
-elif (cmd == '-w'):
-    write()
-    read(2)
-else:
-    clear()
+
+print("[rwc] [filename or size] [address]")
+print("q to quit")
+
+cmd = input("Command: ")
+while (cmd != 'q'):
+    args = cmd.split()
+    if (args[0] == 'r'):
+        size = convert(args[1])
+        addr = convert(args[2])
+        read(size, addr)
+    elif (args[0] == 'w'):
+        file_name = args[1]
+        addr = convert(args[2])
+        write(filename, addr)
+    elif (args[0] == 'c')
+        clear()
+
+    cmd = input("Command: ")
 
 ard.close()
--- code.py
+++ code.py
@@ -1,6 +1,14 @@
 from choc_as import *
 
-add(r1, r2)
-add(r1, r3)
+ln(r0)  # 0x0
+word(0x8)
+jz(r0, r0)
+nop()  
+nop()  # 0x4
+nop()
+nop()
+nop()
+jz(r1, r1)  # 0x8
+
 
 qcompile('code.hex')
